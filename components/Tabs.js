3ca2453fd4d62a67a9098781e8e70fed8d930813
Mary Baker <meb601@gmail.com>
2020-08-07 12:41:33 -0400

    Create createTab function

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 // STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
@@ -9,3 +11,54 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+// gitHubProfileArray.forEach(username => {
+//     const profileURL = 'https://api.github.com/users/' + username;
+//     axios.get(profileURL)
+//   .then(response => {
+//     createCard(response.data);
+//     })
+//     .catch(error => {
+//       console.log("The data was not returned.", error)
+//     });
+//   });
+
+
+
+// axios.get('https://lambda-times-api.herokuapp.com/topics')
+// .then(response => {
+//     console.log(response)
+//     const newspaperTabs = response.data.topics;
+//     newspaperTabs.forEach(tab => {
+//         createTab()
+//     })
+//     console.log(newspaperTabs);
+// })
+//     .catch(error => {
+//         console.log("The data was not returned. ", error)
+//     });
+
+//    <div class="tab">topic here</div>
+
+    function createTab() {
+    
+        //Create Element
+        const tabs = document.createElement('div');
+    
+        //Add Class to Element
+        tabs.classList.add('topic');
+    
+        //Text Content
+        tabs.textContent = 'TBD';
+    
+        //Select Header Container Element
+        const tabsContainer = document.querySelector('.tabs');
+    
+        //Append Elements
+        tabsContainer.append(tabs);
+    
+    return tabs;
+    
+    }
+    
+    createTab();
