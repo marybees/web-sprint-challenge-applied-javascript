2245e6c3b0f613256911b516a185c46852c831c0
Mary Baker <meb601@gmail.com>
2020-08-07 14:39:20 -0400

    Update GET request

--- components/Cards.js
+++ components/Cards.js
@@ -24,18 +24,29 @@
 const { default: Axios } = require("axios");
 
 axios.get('https://lambda-times-api.herokuapp.com/articles')
-.then (response => {
+.then((response) => {
     console.log(response)
+    let articlesKeys = Object.keys(response.data.articles);
+    articlesKeys.forEach((key) => {
+        console.log(key)
+        const articles = response.data.articles[key]
+        articles.forEach((article) => {
+            createCard(article)
+        })
+    });
 })
+.catch((error) => {
+    console.log("The data was not returned. ", error);
+});
 
 function createCard(article) {
-    
+console.log(article)
     //Create Element
     const card = document.createElement('div')
     const headline = document.createElement('div')
     const author = document.createElement('div')
     const imgContainer = document.createElement('div')
-    const authorImg = document.createElement('img'),
+    const authorImg = document.createElement('img')
     const authorName = document.createElement('span')
 
     //Add Class to Element
@@ -71,5 +82,3 @@ function createCard(article) {
 return card;
 
 }
-
-createCard();
